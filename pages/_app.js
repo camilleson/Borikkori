941ceefe51bc2bcce061b00f5f6b439d627eab3b
camilleson <asa.son59@gamil.com>
2022-09-24 16:15:41 +0900

    styled-component detail 정리 및 수정

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,15 @@
 import { ThemeProvider } from 'styled-components';
-import { RecoilRoot } from 'recoil';
 import Nav from '../components/Navbar';
 import GlobalStyle from '../styles/global-styles';
 import theme from '../styles/theme';
-
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <RecoilRoot>
         <Nav/>
         <Component {...pageProps} />
-      </RecoilRoot>
     </ThemeProvider>
   );
 }
